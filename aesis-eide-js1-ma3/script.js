b2b80210b1d83f32046fe083f51228f5a8417cc5
Aesiseide <91593840+Aesiseide@users.noreply.github.com>
2022-02-14 23:55:32 +0100

    Update script.js

--- aesis-eide-js1-ma3/script.js
+++ aesis-eide-js1-ma3/script.js
@@ -1,3 +1,7 @@
+// Question 1
+const getRemainder = (a,b) =>  return a % b;
+
+// Question 2
 const url =
   "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=05220d87e9204a108c5c645b089d2af8";
 
